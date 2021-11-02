03acb592d055d86c2189653cf5c51cd130e1fbbb
irancssofficial <irancssofficial@gmail.com>
2021-11-02 19:26:34 +0330

    Commit Write IGenericRepository Interface

---
+++ Repository/GenericRepository.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+using HotelListing.Data;
+using HotelListing.IRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.Repository
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private DataBaseContext _context;
+        private readonly DbSet<T> _db;
+
+        public GenericRepository(DataBaseContext context)
+        {
+            _context = context;
+            _db = _context.Set<T>();
+        }
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteRange(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> Get(Expression<Func<T, bool>> expression, List<string> includes = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IList<T>> GetAll(Expression<Func<T, bool>> expression = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, List<string> includes = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Insert(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task InsertRange(IEnumerable<T> entities)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
