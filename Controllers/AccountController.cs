2ac145da25b32f26145fe87810491c6899f94547
irancssofficial <irancssofficial@gmail.com>
2021-11-03 21:20:29 +0330

    Commit add AccountController and UserDTO

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using HotelListing.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+
+namespace HotelListing.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        private IMapper _mapper;
+        private ILogger<AccountController> _logger;
+        private SignInManager<ApiUser> _signInManager;
+        private UserManager<ApiUser> _userManager;
+
+        public AccountController(IMapper mapper, ILogger<AccountController> logger, SignInManager<ApiUser> signInManager, UserManager<ApiUser> userManager)
+        {
+            _mapper = mapper;
+            _logger = logger;
+            _signInManager = signInManager;
+            _userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] UserDTO userDTO)
+        {
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
---
+++ Models/UserDTO.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Models
+{
+    public class UserDTO
+    {
+        [StringLength(maximumLength:30,ErrorMessage = "your firstName is too Long")]
+        public string FirstName { get; set; }
+        [StringLength(maximumLength: 30, ErrorMessage = "your lastName is too Long")]
+        public string LastName { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(15,ErrorMessage = "your password is limited to {2} to {1} characters",MinimumLength = 7)]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
