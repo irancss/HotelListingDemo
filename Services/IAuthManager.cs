29ddbbeb68035fff8cc6cd79656d24d1d0f04bb8
irancssofficial <irancssofficial@gmail.com>
2021-11-04 00:46:17 +0330

    Commit add Interface IAuthManager

---
+++ Services/IAuthManager.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HotelListing.Models;
+using Serilog;
+
+namespace HotelListing.Services
+{
+    public interface IAuthManager
+    {
+        Task<bool> ValidateUser(LoginUserDTO loginUserDto);
+        Task<string> CreateToken();
+    }
+}
