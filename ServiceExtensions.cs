0cde34a9d520e3abe19234176dd0df2eef5234e4
irancssofficial <irancssofficial@gmail.com>
2021-11-04 00:41:44 +0330

    Commit add ConfigureJwt Extension

--- ServiceExtensions.cs
+++ ServiceExtensions.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using AutoMapper.Configuration;
 using HotelListing.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace HotelListing
 {
@@ -21,9 +26,35 @@ namespace HotelListing
                 q.Password.RequiredLength = 5;
             });
 
-            builder = new IdentityBuilder(builder.UserType, typeof(IdentityRole),services);
+            builder = new IdentityBuilder(builder.UserType, typeof(IdentityRole), services);
             builder.AddEntityFrameworkStores<DataBaseContext>().AddDefaultTokenProviders();
-
         }
+
+        #region Extenstion Method for JWT Config
+
+        public static void ConfigureJwt(this IServiceCollection services, Microsoft.Extensions.Configuration.IConfiguration configuration)
+        {
+            var jwtSettings = configuration.GetSection("jwt");
+            var key = Environment.GetEnvironmentVariable("KEY");
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateIssuer = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = jwtSettings.GetSection("Issuer").Value,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key))
+                };
+            });
+        }
+        #endregion
+
     }
 }
