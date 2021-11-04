f9b3531fded91e62d31a8caf25875c2d50878679
irancssofficial <irancssofficial@gmail.com>
2021-11-05 00:32:13 +0330

    Commit add RequestParams for pagInation

---
+++ Models/RequestParams.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HotelListing.Models
+{
+    public class RequestParams
+    {
+        private const int MaxPageSize = 50;
+
+        public int PageNumber { get; set; } = 1;
+        public int _pageSize { get; set; } = 10;
+
+        public int PageSize
+        {
+            get { return _pageSize; }
+            set { _pageSize = (value > PageSize) ? MaxPageSize : value; }
+        }
+
+    }
+}
